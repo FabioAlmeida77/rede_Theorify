589ac9db04fa6836084ee6f71a483b5bb9c392f2
fabio almeida <fabioalmeidadeoliveira712@gmail.com>
2025-07-03 22:51:00 -0300

    dando permição com o commit

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
+app.use(cors({
+  origin: ['https://theorify-front.vercel.app'], 
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
